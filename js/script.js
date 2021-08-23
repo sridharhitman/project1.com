d674116d76cb07b694358017967d374a614a4809
sridharhitman <83956232+sridharhitman@users.noreply.github.com>
2021-08-23 15:00:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// vars
+var first_name = " Sridhar";
+var last_name = "Hisaria";
+var convert_uppercase = first_name.toUpperCase();
+var concat = first_name.concat(last_name);
+var a = 81;
+var b = 92;
+var squareroot = Math.sqrt(a);
+var power = Math.pow(8, 8);
+var wrong = " The class was very bad";
+var replaced = wrong.replace("bad", "good");
+//consoles
+console.log(first_name);
+console.log(last_name);
+console.log(convert_uppercase)
+console.log(a);
+console.log(b);
+console.log(concat);
+console.log(squareroot);
+console.log(power);
+console.log(wrong);
+console.log(replaced)
